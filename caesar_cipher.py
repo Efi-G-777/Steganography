bffc0a65ce2745d00e37dc24795927176b5a80ce
mefathim-77 <efigoldberg@gmail.com>
2025-04-02 11:49:16 +0300

    added caesar cipher with multiple keys, 1024-bit rsa keys generating

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -14,3 +14,20 @@ def caesar_cipher(text, shift):
 def caesar_decipher(text, shift):
     return caesar_cipher(text, -shift)
 
+def caesar_cipher_mult(text, key):
+    result = ""
+    shift = 0
+    for char in text:
+        if char.isalpha():
+            shift_amount = key[shift] % 26
+            new_char = chr(((ord(char.lower()) - ord('a') + shift_amount) % 26) + ord('a'))
+            if char.isupper():
+                new_char = new_char.upper()
+            result += new_char
+            shift = (shift + 1) % (len(key) - 1)
+        else:
+            result += char
+    return result
+
+print(caesar_cipher_mult("Hello world", (1, 2, 3)))
+
--- rsa_generate.py
+++ rsa_generate.py
@@ -1,4 +1,4 @@
-from sympy import mod_inverse
+from sympy import mod_inverse, randprime
 from config import *
 import random
 
@@ -11,4 +11,22 @@ def generate_rsa_keys():
     print("Public key: ", (e, n))
     print("Private key: ", (d, n))
 
-generate_rsa_keys()
+# generate_rsa_keys()
+
+# lower_bound = 2**1023  # Smallest 1024-bit number
+# upper_bound = 2**1024 - 1  # Largest 1024-bit number
+#
+# prime = randprime(lower_bound, upper_bound)
+# print(prime)
+
+def generate_large_rsa_keys():
+    lower_bound = 2 ** 1023
+    upper_bound = 2**1024 - 1
+    p1, p2 = randprime(lower_bound, upper_bound), randprime(lower_bound, upper_bound)
+    n = p1 * p2
+    t_n = (p1 - 1) * (p2 - 1)
+    e = randprime(2 ** 64, 2 ** 128)
+    d = mod_inverse(e, t_n)
+    print("Public key: ", (e, n))
+    print("Private key: ", (d, n))
+generate_large_rsa_keys()
